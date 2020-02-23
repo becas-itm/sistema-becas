52eb150072d19e1930254bfa651e6421dec48f16
salchichongallo <salchichongallo1997@gmail.com>
2020-02-23 10:09:09 -0500

    feat(documents): add scholarship document

---
+++ itm/documents.py
@@ -0,0 +1,33 @@
+import elasticsearch_dsl as dsl
+
+
+class Scholarship(dsl.Document):
+    class Index:
+        name = 'scholarships'
+
+    @property
+    def id(self):
+        return self.meta.id
+
+    name = dsl.Text(required=True)
+
+    description = dsl.Text()
+
+    deadline = dsl.Date()
+
+    state = dsl.Keyword(required=True)
+
+    createdAt = dsl.Date(required=True)
+
+    approval = dsl.Object(
+        properties={
+            'approvedAt': dsl.Date(required=True),
+        },
+    )
+
+    denial = dsl.Object(
+        properties={
+            'deniedAt': dsl.Date(required=True),
+            'reason': dsl.Text(required=True),
+        },
+    )
