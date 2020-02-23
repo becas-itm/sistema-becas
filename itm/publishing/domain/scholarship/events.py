b8235123e478acaf950886588b18c73b724d5b63
salchichongallo <salchichongallo1997@gmail.com>
2020-02-23 09:35:25 -0500

    refactor(publishing): use instance variables instead of properties

--- itm/publishing/domain/scholarship/events.py
+++ itm/publishing/domain/scholarship/events.py
@@ -3,20 +3,12 @@ from datetime import datetime
 
 class ScholarshipApproved:
     def __init__(self, scholarship_id, timestamp):
-        self._scholarship_id = scholarship_id
-        self._timestamp = timestamp
+        self.scholarship_id = scholarship_id
+        self.timestamp = timestamp
 
     @classmethod
     def fire(cls, scholarship_id):
         return cls(scholarship_id, datetime.utcnow())
-
-    @property
-    def scholarship_id(self):
-        return self._scholarship_id
-
-    @property
-    def timestamp(self):
-        return self._timestamp
 
 
 class ScholarshipDenied:
