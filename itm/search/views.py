e9754e778f0b4607e2f4f7c598f8ffdf9e5c5fa0
salchichongallo <salchichongallo1997@gmail.com>
2020-02-26 11:25:35 -0500

    feat(search): implement search scholarships view

--- itm/search/views.py
+++ itm/search/views.py
@@ -1,7 +1,30 @@
 from django.views.decorators.http import require_GET
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseForbidden
+
+from itm.shared.utils import SimplePaginator
+from itm.publishing.domain.scholarship import State
+
+from .search import SearchBuilder
+from .service import SearchService
 
 
 @require_GET
 def search(request):
-    return JsonResponse({})
+    try:
+        page = int(request.GET.get('page', 1))
+        assert(page >= 1)
+    except (ValueError, AssertionError):
+        return HttpResponseForbidden('Invalid page')
+
+    paginator = SimplePaginator(page)
+
+    builder = SearchBuilder() \
+        .select(['name', 'description', 'deadline', 'spider.name', 'entity.fullName']) \
+        .size(paginator.per_page) \
+        .skip(paginator.skip) \
+        .with_state(State.PUBLISHED.value)
+
+    if 'q' in request.GET:
+        builder.add_term(request.GET['q'])
+
+    return JsonResponse(paginator.paginate(SearchService.execute(builder)))
