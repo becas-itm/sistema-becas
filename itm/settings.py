e59cf697313d6b4f5ca9b115f71ffd6d4bbd9218
Stiven Diaz <stivendiaz@MNB112.local>
2020-02-29 12:24:27 -0500

    chore: add rest framework

--- itm/settings.py
+++ itm/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'itm.search.apps.SearchConfig',
 ]
 
