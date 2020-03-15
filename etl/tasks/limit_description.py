f84b35c8eaac6c3353e485cadf63cea57af25cdb
salchichongallo <salchichongallo1997@gmail.com>
2020-03-15 10:24:50 -0500

    refactor(limit_description): use first sentence

--- etl/tasks/limit_description.py
+++ etl/tasks/limit_description.py
@@ -1,13 +1,28 @@
-MAX_CHARACTERS = 200
+import spacy
+
+
+from etl.common import Language
+
+es_nlp: spacy.tokens.Doc = spacy.load('es_core_news_sm')
+en_nlp = spacy.load('en_core_web_sm')
 
 
 def limit_description(item: dict):
-    if 'description' not in item:
+    if 'description' not in item or 'language' not in item:
         return item
 
-    if (len(item['description']) > MAX_CHARACTERS):
-        ellipsis = '...'
-        maxCharacters = MAX_CHARACTERS - len(ellipsis)
-        item['description'] = item['description'][:maxCharacters] + ellipsis
+    item['description'] = extract_first_sentence(item['description'], item['language'])
 
     return item
+
+
+def extract_first_sentence(text, language):
+    language = Language(language)
+
+    doc = None
+    if language == Language.SPANISH:
+        doc = es_nlp(text)
+    elif language == Language.ENGLISH:
+        doc = en_nlp(text)
+
+    return str(list(doc.sents)[0]) if doc else text
