11c0626ac22026425266ad0eff674b928efabe94
salchichongallo <salchichongallo1997@gmail.com>
2020-03-22 13:07:16 -0500

    chore: remove django support

--- itm/publishing/infrastructure/api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PublishingConfig(AppConfig):
-    name = 'itm.publishing.infrastructure.api'
--- itm/search/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-
-urlpatterns = [
-    path('scholarships/', views.search),
-    path('scholarships/<scholarship_id>/', views.search_detail),
-]
