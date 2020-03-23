c50915270662f416804f08b16c64ffb67deb041c
salchichongallo <salchichongallo1997@gmail.com>
2020-03-22 21:04:43 -0500

    feat(countries): get country name by code

---
+++ itm/shared/utils/countries/__init__.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+def load_file(filename):
+    with open(os.path.join(BASE_PATH, filename), 'r') as file:
+        return json.loads(file.read())
+
+
+def get_country_name(code):
+    countries = load_file('country_names_by_code.json')
+    return countries.get(code)
