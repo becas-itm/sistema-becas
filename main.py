58c5aac6a2837ad3c857bb57145c4a93e1c194f5
salchichongallo <salchichongallo1997@gmail.com>
2020-04-05 11:07:03 -0500

    chore: init app from main module

--- main.py
+++ main.py
@@ -27,3 +27,8 @@ app.include_router(users_router,
                    dependencies=[Depends(verify_token)])
 
 app.include_router(auth_router, prefix='/api/auth')
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host=os.getenv('APP_HOST', '0.0.0.0'), port=int(os.getenv('APP_PORT', 3001)))
