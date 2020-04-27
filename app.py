0f1aca2f5bf76b7dcd2519a7ad6bf6ff61ad2e1e
salchichongallo <salchichongallo1997@gmail.com>
2020-04-27 13:03:29 -0500

    refactor: separate main module from app

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+
+from fastapi import FastAPI, Depends
+
+from itm.shared.utils.auth import verify_token
+from itm.search.routes import router as search_router
+
+from itm.users.auth import router as auth_router
+from itm.users.routes import router as users_router
+from itm.publishing.infrastructure.api.routes import router as publishing_router
+
+
+app = FastAPI()
+
+
+@app.get('/api/ping')
+def ping_status():
+    return {'status': "It's alive"}
+
+
+app.include_router(search_router, prefix='/api/search/scholarships')
+
+app.include_router(publishing_router,
+                   prefix='/api/publishing/scholarships',
+                   dependencies=[Depends(verify_token)])
+
+app.include_router(users_router,
+                   prefix='/api/users',
+                   dependencies=[Depends(verify_token)])
+
+app.include_router(auth_router, prefix='/api/auth')
--- main.py
+++ main.py
@@ -1,36 +1,11 @@
 import os
 
-from fastapi import FastAPI, Depends
-
 from elasticsearch_dsl.connections import create_connection
 
-from itm.shared.utils.auth import verify_token
-from itm.search.routes import router as search_router
-
-from itm.users.auth import router as auth_router
-from itm.users.routes import router as users_router
-from itm.publishing.infrastructure.api.routes import router as publishing_router
-from itm.ping.routes import router as ping_router
+from app import app
 
 
 elastic = create_connection(alias='default', hosts=[os.getenv('ELASTIC_HOST', '127.0.0.1')])
-
-app = FastAPI()
-
-app.include_router(ping_router, prefix='/api/ping')
-
-app.include_router(search_router, prefix='/api/search/scholarships')
-
-app.include_router(publishing_router,
-                   prefix='/api/publishing/scholarships',
-                   dependencies=[Depends(verify_token)])
-
-app.include_router(users_router,
-                   prefix='/api/users',
-                   dependencies=[Depends(verify_token)])
-
-app.include_router(auth_router, prefix='/api/auth')
-
 
 if __name__ == '__main__':
     import uvicorn
