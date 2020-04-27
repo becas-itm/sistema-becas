40da6c21d5cc9156127c848fe4173932f8fc115c
salchichongallo <salchichongallo1997@gmail.com>
2020-04-27 12:33:18 -0500

    refactor: use api fixture with scoped session

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,8 +1,10 @@
 import os
 
 import pytest
-
+from fastapi.testclient import TestClient
 from pytest_elasticsearch import factories
+
+from main import app
 
 from elasticsearch_dsl.connections import add_connection
 
@@ -15,3 +17,8 @@ elasticsearch = factories.elasticsearch('elasticsearch_nooproc')
 @pytest.fixture(autouse=True)
 def init_db(elasticsearch):
     add_connection('default', elasticsearch)
+
+
+@pytest.fixture(scope='session')
+def api():
+    return TestClient(app)
