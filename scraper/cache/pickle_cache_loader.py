559d98e6fb584d8285b68250ec49e73ad9fa828e
salchichongallo <salchichongallo1997@gmail.com>
2020-05-01 16:58:33 -0500

    feat(scraper): add cache loader
    Co-authored-by: stivendiaz <stivendiazagudelo@gmail.com>

---
+++ scraper/cache/pickle_cache_loader.py
@@ -0,0 +1,24 @@
+import pickle
+
+
+FILE_MODE_WRITE_BINARY = 'wb'
+FILE_MODE_READ_BINARY = 'rb'
+
+
+class PickleCacheLoader:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def save(self, data):
+        with self.open_file(FILE_MODE_WRITE_BINARY) as file:
+            pickle.dump(data, file)
+
+    def load(self):
+        try:
+            with self.open_file(FILE_MODE_READ_BINARY) as file:
+                return pickle.load(file)
+        except FileNotFoundError:
+            return {}
+
+    def open_file(self, modes):
+        return open(self.filename, modes)
