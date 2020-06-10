a002c2036707571b9063efa8c15fe9ab9eb864e2
stivendiaz <stivendiazagudelo@gmail.com>
2020-06-09 22:23:44 -0500

    refactor: change entity structure

--- etl/tasks/add_entity_fields.py
+++ etl/tasks/add_entity_fields.py
@@ -2,11 +2,11 @@ from etl.common.entities import get_entity_full_name
 
 
 def add_entity_fields(item: dict):
-    name = item['spider']['name']
+    code = item['spider']['name']
 
     item['entity'] = {
-        'name': name,
-        'fullName': get_entity_full_name(name),
+        'code': code,
+        'name': get_entity_full_name(code),
     }
 
     return item
